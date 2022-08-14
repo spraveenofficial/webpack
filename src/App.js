dadf5b103080eede0f67a90e827216ccd4f0ddcc
Praveen <spraveen593@gmail.com>
2022-08-15 01:14:13 +0530

    feat: :sparkles: Added Code Splitting aka Code Bundling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
 import img from "./img.png";
+import _ from "lodash";
 
 const App = ({ name }) => {
     return `
     <img src="${img}" alt=""/>
     <h2> Welcome, ${name} </h2>
+    <h2> ${_.join(["Testing,", name], " ")} </h2>
     `;
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const webpackConfig = {
+    // Add source map support aka entry point for webpack dev server
     entry: path.resolve(__dirname, "src", "index.js"),
 
     output: {
-        filename: "main.js",
         path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+        // Added Clean, this will remove the dist folder before building
         clean: true,
-
     },
     module: {
         rules: [
@@ -22,22 +23,39 @@ const webpackConfig = {
                     }
                 }
             },
+            // Added this to load the css files
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"]
             },
+            // Added this to load images
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: "asset"
             }
         ]
     },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                node_vendors: {
+                    name: "vendor",
+                    test: /[\\/]node_modules[\\/]/,
+                    chunks: "all",
+                    priority: 1
+                }
+            }
+        }
+    },
+    // Plugin for HTML Document Title
     plugins: [
         new HtmlWebpackPlugin({
             title: "Webpack Configured App",
             template: path.resolve(__dirname, "src", "index.html")
         })
     ],
+
+    // Added this to enable hot reloading
     mode: "development",
 
 };
